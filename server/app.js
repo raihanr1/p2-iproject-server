1f5a4daff406ba3152910bf6b1e9f4d17efd6401
raihanr1 <raihanrobbani123@gmail.com>
2021-11-17 00:31:46 +0700

    set up routes

--- server/app.js
+++ server/app.js
@@ -1,17 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
 const router = require("./routes/index");
 const cors = require("cors");
-require("dotenv").config();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("lalallaand");
-});
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,61 @@
+const axios = require("axios");
 const express = require("express");
 const router = express.Router();
+router.post("/", async (req, res) => {
+  try {
+    // create VA
+    let { bank_code, name } = req.body;
+    let response = await axios({
+      method: "post",
+      url: "https://api.xendit.co/callback_virtual_accounts",
+      data: {
+        external_id: `VA_fixed-${new Date().getTime()}`,
+        bank_code,
+        name,
+      },
+      auth: {
+        username: `${process.env.PRIVATE_KEY_XENDIT}`,
+        password: "password",
+      },
+    });
+    res.send(response.data);
+  } catch (error) {
+    res.json(error);
+    console.log(error, ">>>");
+  }
+});
 
-router.get("/");
+router.get("/VA_ACC", async (req, res) => {
+  try {
+    // Check VA
+    let response = await axios({
+      method: "get",
+      url: `https://api.xendit.co/callback_virtual_accounts/61939d5ef932ce65c128517c`,
+      auth: {
+        username: `${process.env.PRIVATE_KEY_XENDIT}`,
+        password: "password",
+      },
+    });
+    res.send(response.data);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.get("/VA_BANK", async (req, res) => {
+  try {
+    let response = await axios({
+      method: "get",
+      url: "https://api.xendit.co/available_virtual_account_banks",
+      auth: {
+        username: `${process.env.PRIVATE_KEY_XENDIT}`,
+        password: "password",
+      },
+    });
+    res.send(response.data);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 module.exports = router;
